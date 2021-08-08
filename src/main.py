53a187427999d9c99590e59922f1a66b1106fb44
Max Manni <manni.massimiliano@gmail.com>
2021-08-08 15:16:48 +0200

    ping test first commit

--- src/main.py
+++ src/main.py
@@ -3,15 +3,54 @@ import sys
 import requests
 import subprocess
 import logging
+import threading
+
+from ping3 import ping
+
 from parrocchie_valmalenco_fm.utils import get_current_date, get_current_time
 from parrocchie_valmalenco_fm.orari_config import OrariConfig
 from parrocchie_valmalenco_fm.config import Config
 from parrocchie_valmalenco_fm.camera_config import CameraConfig
 
-
 ORARI_FILE = 'orari.csv'
 RELAY_CONFIG_FILE = 'config-relay.ini'
 CAMERA_CONFIG_FILE = 'config.ini'
+
+nodes = {
+    'home': {
+        'address': '127.0.0.1',
+        'lastPing': False
+    },
+    'another_node': {
+        'address': '192.168.53.23',
+        'lastPing': False
+    }
+}
+
+
+def background_ping(ping_id):
+    logging.info('Ping thread on node ' + ping_id + ' started')
+    while True:
+        address = nodes[ping_id]['address']
+        old_ping = nodes[ping_id]['lastPing']
+
+        try:
+            ret = ping(dest_addr=address, timeout=3)
+            if ret is None:
+                new_ping = False
+            else:
+                new_ping = True
+        except ping.EXCEPTIONS:
+            new_ping = False
+
+        if not old_ping and new_ping:
+            logging.info('Node ' + ping_id + ' goes UP')
+        if old_ping and not new_ping:
+            logging.info('Node ' + ping_id + ' goes DOWN')
+
+        nodes[ping_id]['lastPing'] = new_ping
+        time.sleep(3)
+
 
 if __name__ == '__main__':
 
@@ -34,6 +73,16 @@ if __name__ == '__main__':
 
     logging.info('Program started')
 
+    # to start the ping threads:
+    # pythonw main.py random_string
+    # watch the last lines of the log:
+    # tail -n 15 -F app.log
+    if len(sys.argv) > 1:
+        logging.info("Starting ping test threads")
+        for node_id in nodes.keys():
+            x = threading.Thread(target=background_ping, args=(node_id,))
+            x.start()
+
     while True:
         current_date = get_current_date()
         current_time = get_current_time()
@@ -45,7 +94,7 @@ if __name__ == '__main__':
 
             # build the streaming url
             url = Config.get_streaming_url(cameras.ip_dict[active_slot], cameras.port_dict[active_slot])
-            path_audio_start = sigla_path_dict[active_slot+'_inizio.mp3']
+            path_audio_start = sigla_path_dict[active_slot + '_inizio.mp3']
             path_audio_end = sigla_path_dict[active_slot + '_fine.mp3']
 
             # the url is now eligible to be started. However, until the endpoint is not reachable, it can't be started
@@ -98,7 +147,7 @@ if __name__ == '__main__':
                                              " --novideo"], shell=True)
 
                 logging.info("Started listening from " + url + " at " + current_time)
-                logging.info("Started listening from "+url+" at "+current_time)
+                logging.info("Started listening from " + url + " at " + current_time)
                 pid_vlc = proc.pid
                 streaming_started = True
 
@@ -121,7 +170,7 @@ if __name__ == '__main__':
 
                     # 2° Step: play the stop event announcement
                     subprocess.Popen(['/Applications/VLC.app/Contents/MacOS/VLC ' + path_audio_end +
-                                             ' vlc://quit'], shell=True)
+                                      ' vlc://quit'], shell=True)
 
                     logging.info('Playing ' + path_audio_end)
 
@@ -156,7 +205,7 @@ if __name__ == '__main__':
 
                 # 2° Step: play the stop event announcement
                 subprocess.Popen(["powershell.exe", "C:/'Program Files'/VideoLAN/VLC/vlc.exe "
-                                   + path_audio_end + " vlc://quit"], shell=True)
+                                  + path_audio_end + " vlc://quit"], shell=True)
 
                 logging.info('Playing ' + path_audio_end)
 
